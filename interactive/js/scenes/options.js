c83b2bce6feba9efc9c3160d56f38df90440a98a
Nick Field <heyimnick@live.com>
2017-02-01 17:36:06 +0000

    add options scene
    Related #19

---
+++ interactive/js/scenes/options.js
@@ -0,0 +1,89 @@
+// BackgroundColor = #26C6DA
+// TextColor = #FAFAFA
+
+// Initial Styles
+var turnHeadToOptions = {
+  opacity: 1,
+  top: "195px",
+  right: "67px",
+  width: "11%"
+}
+
+var practicalMirror = {
+  opacity: 1,
+  top: "45%",
+  left: "15%"
+}
+
+var bDogBody = {
+  opacity: 1,
+  top: "50%",
+  left: "38%",
+  width: "13%"
+}
+var bDogHead = {
+  opacity: 1,
+  top: "35%",
+  left: "40%",
+  width: "11%"
+}
+
+var dogBowlOption = {
+  opacity: 1,
+  top: "61%",
+  right: "30%",
+  width: "9%"
+}
+/* Text Styles */
+var practicalOptionText = {
+  opacity: 1,
+  top: "67%",
+  left: "12%"
+}
+var talkingOptionText = {
+  opacity: 1,
+  top: "67%",
+  left: "36%"
+}
+var medicalOptionText = {
+  opacity: 1,
+  top: "67%",
+  left: "57%"
+}
+
+
+
+// Event
+$("#dogheadfacingfront").on("click", function() {
+  if (globalState.currentscene == "introduction") {
+    optionsScene();
+  }
+});
+
+// 1
+function optionsScene () {
+  TweenMax.to("#imgcontainer", 1, {backgroundColor: "#26C6DA"});
+
+  var tl = new TimelineLite({onComplete: clickText});
+  tl.to("#textcontainer", 1, {opacity: 0})
+    .to("#dogheadfacingfront", 1, {x: "270px"}, "-=0.7")
+    .to("#dogbodyfacingfront", 1, {x: "260px"}, "-=1.05")
+    .to("#dogheadfacingfront", 0, {opacity: 0})
+    .to("#dogheadfacingleft", 0, turnHeadToOptions)
+    .to("#miscmirrorwithdog", .2, practicalMirror, "+=0.5")
+    .to("#bdogbodyfacingfront", .2, bDogBody)
+    .to("#bdogheadfacingright", .2, bDogHead, "-=0.1")
+    .to("#miscdogbowl", .2, dogBowlOption, "-=0.1")
+    .to("#practicaloptiontext", 0.3, practicalOptionText, "-=.4")
+    .to("#talkingoptiontext", 0.3, talkingOptionText, "-=.2")
+    .to("#medicationoptiontext", 0.3, medicalOptionText, "-=.1");
+}
+
+// 2
+function clickText () {
+  // Change Text First
+  $("#textcontainer").text("Click one of the options to find out more");
+  // Fade in Texts
+  var tl = new TimelineLite();
+    tl.to("#textcontainer", 1, {opacity: 1});
+}
---
+++ interactive/js/scenes/start.js
@@ -0,0 +1,39 @@
+// BackgroundColor = #4FC3F7
+// TextColor = white
+
+// Initial Styles
+var textContainerBottomCenter = {
+  "text-align": "center",
+  "width": "100%",
+  "padding": "0 20%",
+  "left": 0,
+  "bottom": "10%"
+}
+
+var dogBodyCenter = {
+  "bottom": 0,
+  "left": "50%",
+  "width": "13%"
+}
+
+var dogHeadCenter = {
+  "bottom": 0,
+  "left": "50%",
+  "width": "11%"
+}
+
+// Text Pre Set
+$("#textcontainer").css(textContainerBottomCenter);
+$("#textcontainer").text("I'd like to present you with three options for treating Depression");
+
+// Center Dog Pre Set
+$("#dogbodyfacingfront").css(dogBodyCenter);
+$("#dogheadfacingfront").css(dogHeadCenter);
+
+// BG Color
+
+TweenMax.to("#imgcontainer", 0, {backgroundColor: "#4FC3F7"});
+TweenMax.to(["#dogbodyfacingfront", "#dogheadfacingfront"], 0.3, {delay: 1, opacity: 1});
+TweenMax.to("#dogbodyfacingfront", 0.8, {delay: 0.6, top: "50%"});
+TweenMax.to("#dogheadfacingfront", 0.8, {delay: 0.7, top: "37%"});
+TweenMax.to("#textcontainer", 1, {delay: 1.8, color: "white", opacity: 1});
