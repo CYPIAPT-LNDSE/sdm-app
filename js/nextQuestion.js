7c0b64c98d02e9308be48d3f7310e6a5fc07d9f4
denesnori <denes.nori@yahoo.com>
2017-02-01 14:01:59 +0000

    link together the homepages and the cards

--- js/nextQuestion.js
+++ js/nextQuestion.js
@@ -2,14 +2,18 @@ function gotToNextQuestion(id){
   id++;
   let url=window.location.href;
   let base=url.split(/\d/)[0]
+  console.log(url);
+  let newUrl= url.split("/")
+  newUrl.pop();
+  newUrl=newUrl.join('/').replace(" ","");
   if(id<4){
-    location.href=base+"3cards_layout.html?key="+id;
+    location.href=newUrl+"/3cards_layout.html?key="+id;
   }
   else if(id<5){
-    location.href=base+"2cards_layout.html?key="+id;
+    location.href=newUrl+"/2cards_layout.html?key="+id;
   }
   else{
-    location.href=base+"2cards_layout_b.html?key="+id;
+    location.href=newUrl+"/2cards_layout_b.html?key="+id;
   }
   return 0;
 }
